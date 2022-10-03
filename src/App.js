b60f84df5fdc99063827acbab10fe99bbe6fed8e
Cody Crites <cody91494@hotmail.com>
2022-10-03 12:34:17 -0500

    added imports to app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import { React, useState } from "react";
+import Nav from "./containers/Nav/Nav.jsx"
+import Posts from "./containers/Posts/Posts.jsx"
+import PreviousStories from "./containers/PreviousStories.jsx"
+import Profile from "./containers/Profile/Profile.jsx"
+import Reels from "./containers/Reels/Reels.jsx"
+import StoryReplay from "./containers/StoryReplay/StoryReplay.jsx"
+import Tagged from "./containers/Tagged/Tagged.jsx"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
   );
 }
